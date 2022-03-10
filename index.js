14d7fd2d02ff378bbc239595e086fa9ea388aa09
Denis Ivanov <mr.ivanov667@yandex.ru>
2022-03-10 14:30:44 +0500

    preliminary

--- index.js
+++ index.js
@@ -1,49 +1,83 @@
-const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party";
-const token = "e82e29cb73df1c2761b0a06e3e99ac3566b260ca";
-
-
-const addDropdown = (data) => {
-    const li = data.map((item)=>`<li class="li" data-type="li" data-id="${item.data.inn}"><div>${item.value}</div><div>${item.data.inn + ' '+ item.data.address.value}</div></li>`)
-    return ` <ul>${li.join('')}</ul>`
-}
-
 class inputSearch extends HTMLElement{
     constructor() {
         super();
-        this.innerHTML = `<div class="search" id="search"><input value="" type="text"  class="input" placeholder="Введите">
-        <div class="dropdown" id="dropdown"><ul><li></li></ul></div>
-</div>`
-        this.querySelector('div').addEventListener('input', this.search)
-        this.querySelector('div').addEventListener('click', this.select)
+        this.innerHTML = `<div class="search" id="search"></div>`
+        this.el = this.querySelector('div')
+        this.render()
+        this.el.addEventListener('input', this.search)
+        this.el.addEventListener('click', this.select)
     }
+    render(){
+        this.el.innerHTML = `<input value="" type="text"  class="input filedInput" id="input" placeholder="Введите">
+         <div class="dropdown open" id="dropdown"></div>
+         <div class="filedInput"><span>Краткое наименование</span><input value="" type="text"  class="input" placeholder="Краткое наименование"  id="shortName"></div>
+         <div class="filedInput"><span>Полное наименование</span><input value="" type="text"  class="input" placeholder="Полное наименование"  id="fullName"></div>
+         <div class="filedInput"><span>ИНН / КПП</span><input value="" type="text"  class="input" placeholder="INN / КПП"  id="inn"></div>
+         <div class="filedInput"><span>Адрес</span><input value="" type="text"  class="input" placeholder="Адрес"  id="address"></div>
+`
+    }
+    dataSearch = []
+
     select(e){
-        console.log(e.target.parentElement)
+        if(e.target.parentElement.dataset.type == 'li'){
+            let input = document.getElementById('input')
+            let inn = document.getElementById('inn')
+            let shortName = document.getElementById('shortName')
+            let fullName = document.getElementById('fullName')
+            let address = document.getElementById('address')
+
+            let item = this.dataSearch.filter((item)=> item.data.inn === e.target.parentElement.dataset.id)[0]
+
+            inn.value = item.data.inn + " / "+ item.data.kpp
+            input.value = item.value
+            shortName.value = item.data.name.short_with_opf
+            fullName.value = item.data.name.full_with_opf
+            address.value = item.data.address.value
+
+            document.getElementById('dropdown').classList.remove('open')
+        }
     }
     search(){
+        const input = document.getElementById('input')
+        const dropdown = document.getElementById('dropdown')
+        const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party";
+        const token = "e82e29cb73df1c2761b0a06e3e99ac3566b260ca";
 
-        let query = this.children[0].value;
-        const options = {
-            method: "POST",
-            mode: "cors",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                "Authorization": "Token " + token
-            },
-            body: JSON.stringify({query: query})
+        const query = input.value;
+        const add = (data)=>{
+            const li = data.map((item)=>`<li class="li" data-type="li" data-id="${item.data.inn}"><div>${item.value}</div><div class="list">${item.data.inn + ' '+ item.data.address.value}</div></li>`)
+            return ` <ul>${li.join('')}</ul>`
         }
+        if(query){
+            const options = {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": "Token " + token
+                },
+                body: JSON.stringify({query: query})
+            }
+            fetch(url, options)
+                .then(response => response.text())
+                .then(result => {
+                    let data = JSON.parse(result).suggestions
+                    this.dataSearch = data
+                    dropdown.classList.add('open')
+                    data.length ? dropdown.innerHTML =  add(data)
+                        : dropdown.innerHTML = '<ul><li class="li">Нет информации</li></ul>'
+                })
+                .catch(error => console.log("error", error));
+        } else {
+            dropdown.classList.remove('open')
 
-        fetch(url, options)
-            .then(response => response.text())
-            .then(result => {
-                let data = JSON.parse(result).suggestions
-                data.length ? this.children[1].innerHTML =  addDropdown(data)
-                    : this.children[1].innerHTML = ''
+           let inAll = this.querySelectorAll('input')
+            inAll.forEach((item)=>{
+                item.value = ''
             })
-            .catch(error => console.log("error", error));
+        }
     }
-
-
 }
 
 customElements.define("input-search", inputSearch);
